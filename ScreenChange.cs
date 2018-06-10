ae978b3dd81b4616a5dc1e5f6fe981c5df91bb50
BackToThaFutGame <40148780+BackToThaFutGame@users.noreply.github.com>
2018-06-10 21:49:55 +0300

    Screen
    Code for changing scenes in unity

---
+++ ScreenChange.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ScreenChange : MonoBehaviour 
+ {
+    public void PlayPressed() 
+    {
+        SceneManager.LoadScene("SampleScene"); 
+    } 
+    public void ExitPressed()
+    {
+        Application.Quit(); 
+        Debug.Log("Exit pressed!");
+    } 
+    public void MenuPressed() 
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+ }
+ 
