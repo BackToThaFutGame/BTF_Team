3acf11e62d1ca5d8e6933eecc2af91cb12773706
BackToThaFutGame <40148780+BackToThaFutGame@users.noreply.github.com>
2018-06-11 22:54:36 +0300

    Logic for Pause Window
    This code can help you to change windows in your game

---
+++ PauseWindow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseWindow : MonoBehaviour {
+
+    public void StartGamePressed()
+    {
+        SceneManager.LoadScene("SampleScene");
+    } 
+    public void MenuPressed() 
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
