61f91b34a94a3180e0f0274c28cf8f370c84d0e3
BackToThaFutGame <40148780+BackToThaFutGame@users.noreply.github.com>
2018-06-11 22:55:50 +0300

    Add files via upload
    This code can stop your game

---
+++ Stop.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stop : MonoBehaviour {
+    private bool paused = false;
+    public GameObject panel;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.Escape)) 
+        {
+           if (!paused) 
+            {
+                Time.timeScale = 0;
+                paused = true;
+                panel.SetActive(true); 
+            } 
+            else{
+                Time.timeScale = 1;
+                paused = false;
+                panel.SetActive(false);
+            }
+        }
+	}
+}
+ 
